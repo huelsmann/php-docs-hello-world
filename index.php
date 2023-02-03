b501a0c56e4ba673dfdfe0b80813ec35faa98c10
huelsmann <100127962+huelsmann@users.noreply.github.com>
2023-02-03 10:07:11 +0100

    qr-code

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
 echo "Hello Azure?!";
+?>
+<img src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fhls-twin-code.azurewebsites.net%2Fbestellung.php%2F%3Fbnr%3D2002&chs=180x180&choe=UTF-8&chld=L|2" rel="nofollow" alt="qr code"><a href="www.qrcode-generator.de/" border="0" style="cursor:default" rel="nofollow"></a>
